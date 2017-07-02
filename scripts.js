a5ee77fd9afb3b6c2ce6e8fb6a697379fca7ba1a
Latoyya Smith <latoyyas@gmail.com>
2017-07-01 22:43:58 -0500

    updated scripts.js

--- scripts.js
+++ scripts.js
@@ -102,3 +102,33 @@ console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+
+///////////////////// Deep Comparison /////////////////////
+function deepEqual(a,b){
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object")
+    return false;
+  
+  var propsInA = 0, propsInB = 0;
+  
+  for (var prop in a)
+    propsInA += 1;
+  
+  for (var prop in b){
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+  
+  return propsInA == propsInB;
+  
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+
+console.log(deepEqual(obj, {here: 1, object: 2}));
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
